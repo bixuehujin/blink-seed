ec52bb4f5f9073574904107c9bc92ee69c3c8d08
Jin Hu <bixuehujin@gmail.com>
2017-03-15 23:52:39 +0800

    Live reload support for routes, services and plugins

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -10,14 +10,17 @@ $config = require __DIR__ . '/config/app.php';
 /**
  * Loading routes definitions.
  */
-$config['routes'] = require __DIR__ . '/http/routes.php';
+$config['routes'] = __DIR__ . '/http/routes.php';
 
 /**
  * Loading application service definitions.
  */
+$config['services'] = __DIR__ . '/config/services.php';
 
-$config['services'] = require __DIR__ . '/config/services.php';
-
+/**
+ * Loading application plugins definitions.
+ */
+$config['plugins'] = __DIR__ . '/config/plugins.php';
 
 $app = new blink\core\Application($config);
 
